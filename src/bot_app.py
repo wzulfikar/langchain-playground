5b320723f578e368b2249f91417d857d69f579d0
Wildan Zulfikar <hey@wzulfikar.com>
2023-01-26 00:13:52 +0200

    unique chain for each sender id

--- src/bot_app.py
+++ src/bot_app.py
@@ -16,27 +16,35 @@ logging.basicConfig(
 
 class BotApp:
     def __init__(self):
-        template = """Assistant is a large language model trained by OpenAI.
+        self.template = """Assistant is a large language model trained by OpenAI.
   Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
   Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
   Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
   {history}
   Human: {human_input}
   Assistant:"""
-        self.chain = create_chain(template, os.environ.get("VERBOSE") == "1")
+        self.chains = {}
+        self.is_verbose = os.environ.get("VERBOSE") == "1"
 
     async def handle_text(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         """Handle simple message 'ping' to act as a health check"""
         text = update.message.text
 
-        # create variable for the name of the sender
+        sender_id = update.message.from_user.id
         sender_name = update.message.from_user.username or update.message.from_user.first_name
+        logging.info("[message] new message from %s", sender_name)
+
+        # Prepare session for the user
+        chain = self.chains.get(sender_id)
+        if not chain:
+            chain = create_chain(self.template, self.is_verbose)
+            self.chains[sender_id] = chain
+            logging.info("  new chain created: %d", sender_id)
 
         if text == "ping":
             await context.bot.send_message(chat_id=update.effective_chat.id,
                                            text="pong!")
         else:
-            logging.info("[message] new message from %s", sender_name)
             start_time = datetime.datetime.now()
 
             with get_openai_callback() as cb:
@@ -44,7 +52,7 @@ class BotApp:
                                                    action="typing")
                 logging.info("  predicting...")
 
-                output = self.chain.predict(human_input=text)
+                output = chain.predict(human_input=text)
                 bot_reply = output.strip()
                 await context.bot.send_message(chat_id=update.effective_chat.id,
                                                text=bot_reply)
