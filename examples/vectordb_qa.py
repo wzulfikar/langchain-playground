fba1c46c251bf95f0323e99719c1ba5790a5f2a3
Wildan Zulfikar <hey@wzulfikar.com>
2023-02-02 15:03:17 +0200

    format

--- examples/vectordb_qa.py
+++ examples/vectordb_qa.py
@@ -6,27 +6,38 @@ from langchain.vectorstores.faiss import FAISS
 from langchain.chains import VectorDBQAWithSourcesChain
 from langchain import OpenAI
 
-with open('fixtures/state_of_the_union.txt') as f:
-    state_of_the_union = f.read()
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_text(state_of_the_union)
 
-embeddings = OpenAIEmbeddings()
+def create_chain():
+    with open('fixtures/state_of_the_union.txt') as f:
+        state_of_the_union = f.read()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_text(state_of_the_union)
 
-docsearch = FAISS.from_texts(texts, embeddings)
+    embeddings = OpenAIEmbeddings()
 
-# Add in a fake source information
-for i, d in enumerate(docsearch.docstore._dict.values()):
-    d.metadata = {'source': f"{i}-pl"}
+    docsearch = FAISS.from_texts(texts, embeddings)
 
-chain = VectorDBQAWithSourcesChain.from_chain_type(
-    OpenAI(temperature=0), chain_type="stuff", vectorstore=docsearch)
+    # Add in a fake source information
+    for i, d in enumerate(docsearch.docstore._dict.values()):
+        d.metadata = {'source': f"{i}-pl"}
 
-# Start chat loop
-while True:
-    print("Human:")
-    human_input = input()
+    chain = VectorDBQAWithSourcesChain.from_chain_type(
+        OpenAI(temperature=0), chain_type="stuff", vectorstore=docsearch)
 
-    print("AI:")
-    output = chain({"question": human_input}, return_only_outputs=True)
-    print(output)
+    return chain
+
+
+def main():
+    chain = create_chain()
+    # Start chat loop
+    while True:
+        print("Human:")
+        human_input = input()
+
+        print("AI:")
+        output = chain({"question": human_input}, return_only_outputs=True)
+        print(output)
+
+
+if __name__ == "__main__":
+    main()
