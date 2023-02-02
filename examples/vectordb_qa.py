e52ee81e943d86230038f3818ba6c6cc2c0531df
Wildan Zulfikar <hey@wzulfikar.com>
2023-02-02 14:59:43 +0200

    Add vectordb qa

---
+++ examples/vectordb_qa.py
@@ -0,0 +1,32 @@
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.embeddings.cohere import CohereEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.elastic_vector_search import ElasticVectorSearch
+from langchain.vectorstores.faiss import FAISS
+from langchain.chains import VectorDBQAWithSourcesChain
+from langchain import OpenAI
+
+with open('fixtures/state_of_the_union.txt') as f:
+    state_of_the_union = f.read()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+texts = text_splitter.split_text(state_of_the_union)
+
+embeddings = OpenAIEmbeddings()
+
+docsearch = FAISS.from_texts(texts, embeddings)
+
+# Add in a fake source information
+for i, d in enumerate(docsearch.docstore._dict.values()):
+    d.metadata = {'source': f"{i}-pl"}
+
+chain = VectorDBQAWithSourcesChain.from_chain_type(
+    OpenAI(temperature=0), chain_type="stuff", vectorstore=docsearch)
+
+# Start chat loop
+while True:
+    print("Human:")
+    human_input = input()
+
+    print("AI:")
+    output = chain({"question": human_input}, return_only_outputs=True)
+    print(output)
